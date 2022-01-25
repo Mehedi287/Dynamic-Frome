62ec8a2626371a754aaf64ff50ee9236e05da2f7
Mehedi390 <mh0816150@gmail.com>
2022-01-24 21:33:27 -0800

    frist commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import home from './pages/Home/Home';
+import Home from './pages/Home/Home';
+
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+    </div>
+  );
+}
+
+export default App;
